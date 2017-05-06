8513a00bc7c43d021df12ea92c0c5669ece7dc9b
Giancarlos Salas <giansalex@gmail.com>
2017-05-05 22:33:19 -0500

    Init Load

---
+++ AspNetWebApi/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AspNetApiAngular
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "AngularDefault",
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AspNetWebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetApiAngular.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+#if DEBUG
+            return Content("Solo disponible en producion");
+#else
+            return File(HttpContext.Server.MapPath("~/index.html"), "text/html");
+#endif
+        }
+    }
+}
